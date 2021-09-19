f322d552a379b9f3e380361f8c9e7b8522144e31
naimur2 <naimurrahaman248@gmail.com>
2021-09-19 12:23:28 +0600

    express static

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
+app.use(
+    express.static(`${__dirname}/public/`, {
+        index: 'home.html',
+    })
+);
 
 app.get('/', (req, res) => {
     res.send('This is the home page');
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     res.send('This is the home page with post method');
 });
 
