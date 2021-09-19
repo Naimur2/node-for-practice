597ce2066e568d4f403febb68146742fbd4e6341
naimur2 <naimurrahaman248@gmail.com>
2021-09-19 11:54:26 +0600

    changes

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('This is the home page');
 });
 
 app.post('/', (req, res) => {
+    console.log(req.body)
     res.send('This is the home page with post method');
 });
 
 app.listen(3000, () => {
     console.log('listining on port 3000');
 });
-
